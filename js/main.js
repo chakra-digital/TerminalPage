08ce66b1c31c7c48e0c700fc4e1b06928af3625f
chakradojo <info@chakradojo.com>
2021-05-16 13:15:17 -0500

    adding JS

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// we have li id of about & contact, as well as content divs...
+// want to bring those 4 things in
+
+const about = document.querySelector('#about');
+const about = document.querySelector('#contact');
+const about = document.querySelector('#about-content');
+const about = document.querySelector('#contact-content');
