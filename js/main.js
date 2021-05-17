1083a3a182039d2ff27572f238b717e3cc1fce65
chakradojo <info@chakradojo.com>
2021-05-16 21:06:10 -0500

    everything gravy for now

--- js/main.js
+++ js/main.js
@@ -2,6 +2,44 @@
 // want to bring those 4 things in
 
 const about = document.querySelector('#about');
-const about = document.querySelector('#contact');
-const about = document.querySelector('#about-content');
-const about = document.querySelector('#contact-content');
+const contact = document.querySelector('#contact');
+const aboutContent = document.querySelector('.about-content');
+const contactContent = document.querySelector('.contact-content');
+
+about.addEventListener('click', () => {
+  const aboutBox = new WinBox({
+    title: 'About Me',
+    width: '400px',
+    height: '400px',
+    top: 40,
+    right: 40,
+    bottom: 40,
+    left: 40,
+    mount: aboutContent,
+    onfocus: function () {
+      this.setBackground('#00aa00');
+    },
+    onblur: function () {
+      this.setBackground('#777');
+    },
+  });
+});
+
+contact.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Holler',
+    width: '400px',
+    height: '400px',
+    top: 140,
+    right: 40,
+    bottom: 40,
+    left: 240,
+    mount: contactContent,
+    onfocus: function () {
+      this.setBackground('#00aa00');
+    },
+    onblur: function () {
+      this.setBackground('#777');
+    },
+  });
+});
